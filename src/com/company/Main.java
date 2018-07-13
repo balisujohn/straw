60a3c0300848a6562e3f4f7b39f03003d96661f7
John <balis@wisc.edu>
2018-07-12 23:29:07 -0500

    initialization

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+
+//TODO REFACTOR INTO MAIN and RUNTIME so the subclass bullshit works
+
+
+public class Main {
+
+
+
+    public static void main(String args[]) {
+    RuntimeManager run = new RuntimeManager();
+    run.start(args);
+    }
+
+
+}
+
+
+
